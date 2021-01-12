297734ca0a6d7144e67d2d88cda8e9e6a19b7ed7
TopSerg <63195217+TopSerg@users.noreply.github.com>
2021-01-12 17:56:18 +0300

    Update MainActivity.kt

--- app/src/main/java/com/makentoshe/androidgithubcitemplate/MainActivity.kt
+++ app/src/main/java/com/makentoshe/androidgithubcitemplate/MainActivity.kt
@@ -1,12 +1,18 @@
-package com.makentoshe.androidgithubcitemplate
+package com.megaprojectsuperpuper.thecode
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        mainbutton.setOnClickListener{
+            val intent = Intent(this, MainActivity2::class.java)
+            startActivity(intent)
+            //запуска второй активити в вводом кода
+        }
     }
 }
