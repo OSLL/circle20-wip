760a4ffb0dc9b7794758584804032b61cce9ce65
mitya139 <mitcon84@gmail.com>
2021-01-24 20:55:33 +0300

    GLOBALUPLOAD

--- app/src/main/java/com/megaprojectsuperpuper/thecode/Level21.kt
+++ app/src/main/java/com/megaprojectsuperpuper/thecode/Level21.kt
@@ -5,12 +5,12 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+class Level21 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_level21)
         mainbutton.setOnClickListener{
-            val intent = Intent(this, MainActivity2::class.java)
+            val intent = Intent(this, Level22::class.java)
             startActivity(intent)
             //запуска второй активити в вводом кода
         }
